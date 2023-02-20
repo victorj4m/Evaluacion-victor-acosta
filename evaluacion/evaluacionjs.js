12d2b199fc36bc13571591c991cfd4ee489367c9
victorj4m <119911814+victorj4m@users.noreply.github.com>
2023-02-20 00:40:22 -0400

    Add files via upload

---
+++ evaluacion/evaluacionjs.js
@@ -0,0 +1,21 @@
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+// VICTOR ACOSTA CI:30703448
+//--------------------------------------------------------------------------------------------------------------------------------------------
+var Edad=0
+
+
+if(Edad <= 12 && Edad > 0){
+    console.log("Es niñ@")
+}else{
+    if (Edad <= 17 && Edad > 12) {
+        console.log(`"Es Adolecente"`)}
+        else{
+            if (Edad <= 50 && Edad > 18) {
+                console.log(`"Es Adulto"`)}
+                else{if (Edad > 50 && Edad < 50) {
+                    console.log(`"Es Adulto Mayor"`) 
+                }                else {
+                    console.log(`"La Edad Debe Ser Mayor a 0"`);}  }
+            }
+        }
